d069f45cec298c1eb97f8d61b566554dc4411bf2
Gergely Risko <errge@google.com>
2011-04-17 00:35:36 +0200

    Bookmark importer binaries as an example bookmark.

--- userbookmarks_example.js
+++ userbookmarks_example.js
@@ -1,5 +1,6 @@
 hoplax.bookmarks.push(
   { name: "Hoplax project", url: "http://code.google.com/p/hoplax" },
   { name: "Hoplax source code", url: "http://github.com/hoplax/hoplax" },
+  { name: "Hoplax bookmark importer", url: "http://github.com/hoplax/hoplax/tree/binaries" },
   null // for your convenience, so can have the trailing commas in every line
 );
