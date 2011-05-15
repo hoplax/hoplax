8f20cac2558d8cc3f112e6c101d05c485798c190
Gergely Risko <errge@google.com>
2011-05-15 23:48:24 +0200

    Introducing plainHandlers, closes #9.
    A way to generate actions in a completer based solely on user input,
    not on database.  Also added an example to conf_example.js on the
    usage.

--- conf_example.js
+++ conf_example.js
@@ -4,4 +4,20 @@ hoplax.imports = [
         null // Convenience, so you can have commas at the end of every line.
 ];
 
+// Simply jump to the url given, no Google search possible.
+//
+// hoplax.bookmark_completer.plainHandlers = function(s) {
+//   if (s == '')
+//     return [];
+//   else
+//     return [
+//       {
+//         s: 'Jump to: ' + hoplax.urlize(s),
+//         f: function() {
+//           window.location = hoplax.urlize(s);
+//         }
+//       }
+//     ];
+// };
+
 $(document).ready(hoplax.init);
